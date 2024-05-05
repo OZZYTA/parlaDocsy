9e86538d33a041e7e99851a8d5b12c5ab0adf08e
Natali Velasquez <38143334+OZZYTA@users.noreply.github.com>
2024-05-05 14:13:00 -0500

    logo y boton copiar

--- main.py
+++ main.py
@@ -20,13 +20,14 @@ system=f"""Objetivo
   Fecha-> {datetime.datetime.now().strftime("%Y-%m-%d")}\n
   Hora-> {hora_actual}\n
   Nombre de paciente -> Corresponde al nombre del paciente que debe llegar el transcripción recibida, si no llega, debes solicitarlo, es un dato indispensable.\n
+  Edad -> Edad de la paciente.
   Motivo de consulta -> Sintomas o motivo por el cual el paciente consulta, si no llega, debes solicitarlo, es un dato indispensable.\n
   Antecedentes personales -> dentro de los cuales incluye patologicos, quirurgicos, alergicos, toxicos, farmacologicos, vacunas
   Antecedentes familiares -> Incluye posibles enfermedades cronicas de los familiares entre uno y dos grados sanguineos.
   Examen Fisico -> Pruebas que manifiesta el medico haber hecho durante la consulta para poder hacer una impresión diagnóstica
   Análisis -> Corresponde a los procedimientos técnicos hechos en la atención, entre los cuales pueden ser, laboratorios, radiografias, ecografias, electrocardiogramas, examenes generales, etc. Si no hay datos de esto en el prompt, se omite el campo.
   Diagnóstico -> Es el diagnostico que el medico da segun su criterio al paciente, si no llega, debes solicitarlo, es un dato indispensable.\n
-  CIE10 -> Es el codigo segun normativa Cie10 del diagnostico, debes asignarlo automaticamente tomando el diagnostico entregado en el prompt buscandolo en la base en tu conocimiento, sino lo encuentras, omite el campo.\n
+  CIE10 -> Aqui va el codigo CIE10 del diagnostico, lo pones libremente desde tu conocimiento propio, sino lo sabes, omite el campo.\n
   Plan de manejo diagnóstico -> Corresponde a lo que ordena el medico segun el estado del paciente, ya sea dar el alta o salida, dejar en observación, remitir a otro servicios como especialistas o consulta prioritaria o dejar en observación, si no llega, debes solicitarlo, es un dato indispensable.\n
   Tratamiento -> Corresponde al tratamiento medico ordenado compuesto por medicinas, terapias, medicamentos, reposo, incapacidad y posologias, es un campo indispensable, si no llega en el prompt, debes pedirlo.
   Seguimiento -> Corresponde a las acciones recomendadas para aplicar el plan de mejora de la salud del paciente.
@@ -34,7 +35,7 @@ system=f"""Objetivo
   Observaciones -> Data que consideres de caracter disruptivo, inusual o que afecte la atención medica.
 
   Rol
-  Tarea ->Analizar la transcripción recibida y rescatar de ella el texto relevante a contexto medico o clinico y llenar los campos descritos, segun el patron. No repites campos.
+  Tarea ->Analizar la transcripción recibida y rescatar de ella el texto relevante a contexto medico o clinico y llenar los campos descritos, sigue el patrón: Motivo de consulta; Antecedentes personales dentro de los cuales incluye: patologicos, quirurgicos, alergicos, toxicos, farmacologicos, vacunas; Antecedentes familiares; Examen físico; Pruebas diagnósticas; Análisis; Plan de manejo diagnóstico; Seguimiento. No repites campos
   Estilo de conversación-> No conversas, solo llenas el formato.   Deja un salto de linea entre cada uno de los campos listados utilizando "\n" para asignarlos. Formatea tu respuesta de modo que sea profesional, agradable a la vista y facil de entender.
   Personalidad-> Eres serio, profesional y formal y te limitas a ejecutar tu tarea"""
 
@@ -78,8 +79,8 @@ def ask_openai(prompt):
     response = client.chat.completions.create(  
         model="chat-gpt-35-turbo-16k",  
         messages=message_text,  
-        temperature=0.7,  
-        max_tokens=800,  
+        temperature=0.6,  
+        max_tokens=1000,  
         top_p=0.95,  
         frequency_penalty=0,  
         presence_penalty=0,  
